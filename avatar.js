f80725157f80191e63c5a578ac269f300689519b
PappNiki20 <papp.nikoletta001@gmail.com>
2023-05-19 12:59:16 +0200

    kattintasra tortenik valamai-avatar

--- avatar.js
+++ avatar.js
@@ -1,4 +1,14 @@
-list=[
-   "dog.png", "pig.png"
+list = [
+  "dog.png", "pig.png", "avatar_beuke.png"
 ]
 
+$(function () {
+  const ELEM = $(`img`);
+  ELEM.on("click", kattintas);
+
+
+  function kattintas() {
+    let aktELEM = $(event.target).attr("img");
+    $(event.target).attr("src", "pig.png");
+ }
+});
